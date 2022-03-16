9ba32b4e2c35c70078118717bde43ce39f59c9b1
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-17 04:22:36 +0900

    🔧 prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "useTabs": false,
-  "semi": true,
-  "trailingComma": "all",
-  "tabWidth": 2,
-  "singleQuote": false,
-  "printWidth": 100
-}
+  useTabs: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  singleQuote: false,
+  printWidth: 120,
+};
