a836db8e58630dff1796e058f5b0753bc763b787
miyasan_0301 <miyasan.19990301@gmail.com>
2022-09-19 20:48:54 +0900

    :fire:

--- src/pages/api/hello.ts
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-
-const helloAPI = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: "John Doe" });
-};
-// eslint-disable-next-line import/no-default-export
-export default helloAPI;
